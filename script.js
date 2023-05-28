4883ad52607f2f55536768e60eb2eb1d9fd1d925
mahatsumit5 <mahatsumit5@gmail.com>
2023-05-28 23:25:28 +1000

    project finised

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 let taskList = [];
-let hrs = 0;
-let leftHrs = 168;
+let hours = 0;
+let badHours = 0;
+let totalRemainingHours = 168;
 const audioFile = new Audio("./wah.mp3");
 const audioFile1 = new Audio("./beep.mp3");
 const swipe = new Audio("./swipe.mp3");
 var t = document.getElementById("task");
 const entryElm = document.getElementById("entry");
 const badElm = document.getElementById("bad");
-const totalHrs = document.getElementById("total");
-const remainingHrs = document.getElementById("remaining");
+const totalHours = document.getElementById("total");
+const remainingHours = document.getElementById("remaining");
+const hoursToSave = document.getElementById("hoursToSave");
 const totalHoursPerWeek = 24 * 7;
 
 // capture the data from the form on form submit
 // console.log(document.getElementById("myForm").innerText);
 
 const handleOnSubmit = (e) => {
-  let str = "";
-  let strHours = "";
+  let displayString = "";
+  let displayStringHours = "";
 
   const formDt = new FormData(e);
   const task = formDt.get("task");
   const hr = +formDt.get("hr");
   const type = formDt.get("type");
-
+  hours = hours + hr;
   const taskObj = {
     task,
     hr,
@@ -30,27 +32,36 @@ const handleOnSubmit = (e) => {
     type,
   };
 
-  console.log("initial hours check" + hrs);
-  if (hrs > totalHoursPerWeek) {
-    audioFile.play();
-    alert("total maximum hour reached");
-  } else {
-    console.log(hrs);
-    hrs = hrs + hr;
-    leftHrs = leftHrs - parseFloat(hr);
-    // calculationg number of hours
-    if (hrs < totalHoursPerWeek) {
-      taskList.push(taskObj);
-      displayTask();
-      console.log(taskList);
-      str += hrs;
-      strHours += leftHrs;
-      totalHrs.innerHTML = str;
-      remainingHrs.innerHTML = strHours;
-    }
-    audioFile1.play();
-    return hrs;
+  console.log("initial hours check " + hours);
+
+  if (hours > totalHoursPerWeek) {
+    // audioFile.play();
+    // alert("total maximum hour reached");
+    const leftHours = hr - totalRemainingHours;
+    console.log(totalRemainingHours);
+    console.log(leftHours);
+    //if input hours is greater than
+    // left hours value of hours is reduced
+    // because it is added before the function
+    if (leftHours > 0) hours = hours - hr;
+
+    return;
   }
+
+  console.log(hours);
+
+  totalRemainingHours = totalRemainingHours - parseFloat(hr);
+  // calculationg number of hours
+
+  taskList.push(taskObj);
+  displayTask();
+  console.log(taskList);
+  displayString += hours;
+  displayStringHours += totalRemainingHours;
+  totalHours.innerHTML = displayString;
+  remainingHours.innerHTML = displayStringHours;
+
+  audioFile1.play();
 
   // store that data in array as obj
 };
@@ -59,15 +70,15 @@ const handleOnSubmit = (e) => {
 const displayTask = () => {
   const entryList = taskList.filter((item) => item.type === "entry");
 
-  let str = "";
+  let displayString = "";
   entryList.map((item, i) => {
-    str += `
+    displayString += `
 
 <tr>
 <td>${item.task}</td>
 <td>${item.id}</td>
 
-<td>${item.hr} hrs</td>
+<td>${item.hr} hours</td>
 <td>
   <button class="btn btn-danger btn-sm" onclick="deleteTask('${item.id}','${item.hr}')">
     <i class="fa-solid fa-trash"></i>
@@ -80,19 +91,22 @@ const displayTask = () => {
 </td>
 </tr>`;
   });
-  entryElm.innerHTML = str;
+  entryElm.innerHTML = displayString;
   displayBadTask();
 };
 // displying data in the browser
 const displayBadTask = () => {
   const badList = taskList.filter((item) => item.type === "bad");
-
-  let str = "";
+  let displayString = "";
   badList.map((item, i) => {
-    str += `
+    console.log("this is item hours", item.hr);
+    badHours = badHours + item.hr;
+    console.log("total bad hrs", badHours);
+
+    displayString += `
 <tr>
 <td>${item.task}</td>
-<td>${item.hr} hrs</td>
+<td>${item.hr} hours</td>
 <td>
 
 <button class="btn btn-warning btn-sm"
@@ -107,7 +121,12 @@ const displayBadTask = () => {
 </td>
 </tr>`;
   });
-  badElm.innerHTML = str;
+
+  badElm.innerHTML = displayString;
+
+  const badHrs = badList.reduce((acc, item) => acc + +item.hr, 0);
+
+  hoursToSave.innerHTML = "You Could Have Saved " + badHrs + "hrs";
 };
 
 // create unique id
@@ -115,31 +134,31 @@ const displayBadTask = () => {
 const randomGenerator = (lenght = 6) => {
   const collection = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDAZXCVBNM";
 
-  let str = "";
+  let displayString = "";
   for (let i = 0; i < lenght; i++) {
     const ranNum = Math.round(Math.random() * collection.length - 1);
-    str += collection[ranNum];
+    displayString += collection[ranNum];
   }
 
-  return str;
+  return displayString;
 };
 
 // delete item from array based on given id
 
 const deleteTask = (x, h) => {
-  let str = "";
-  let strHours = "";
+  let displayString = "";
+  let displayStringHours = "";
   console.log("the hour to be removed is" + h);
-  console.log(hrs);
+  console.log(hours);
   if (window.confirm("Are you sure you want to delte this?")) {
     taskList = taskList.filter((item) => item.id !== x);
-    hrs = hrs - parseFloat(h);
-    leftHrs = leftHrs + parseFloat(h);
+    hours = hours - parseFloat(h);
+    totalRemainingHours = totalRemainingHours + parseFloat(h);
 
-    str += hrs;
-    strHours += leftHrs;
-    totalHrs.innerHTML = str;
-    remainingHrs.innerHTML = strHours;
+    displayString += hours;
+    displayStringHours += totalRemainingHours;
+    totalHours.innerHTML = displayString;
+    remainingHours.innerHTML = displayStringHours;
     displayTask();
     console.log("hours reduced" + parseInt(h));
   }
